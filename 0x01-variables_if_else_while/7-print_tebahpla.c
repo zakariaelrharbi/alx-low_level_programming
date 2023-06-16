d7650e151c487384bb1254f30211db71ed5d6e3a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-16 16:03:40 -0700

    prints all alphabets reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char x = 'z';
+		while (x <= 'a')
+		{
+			putchar(x);
+			x--;
+		}
+		putchar('\n');
+	return (0);
+}
