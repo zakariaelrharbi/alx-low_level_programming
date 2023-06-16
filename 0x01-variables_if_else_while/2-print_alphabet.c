57bc61c197a934f368cbbcd232288949e2949a7a
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-16 15:03:56 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	return (0);
+}
