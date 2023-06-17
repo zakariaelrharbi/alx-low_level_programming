85e76d8e5674314835e1f3944a5055ef440a7bc7
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-17 09:34:31 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  *
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
 }
