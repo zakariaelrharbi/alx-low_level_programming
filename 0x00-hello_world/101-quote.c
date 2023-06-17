3cdbbf9b0763bb638c8ec59c9106f58720beef07
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-17 09:28:21 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr;
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
 	return (1);
 }
