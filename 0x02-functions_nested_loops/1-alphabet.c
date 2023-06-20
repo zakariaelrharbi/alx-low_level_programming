22b797189562ce5ecfa89b843dc027ea6b106b3d
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-20 15:09:46 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,3 @@ void print_alphabet(void)
 {
 	write(1, "abcdefghijklmnopqrstuvwxyz\n", 28);
 }
-/**
- * main - Entry point
- * function a program that prints lowercase alpha letters.
- * Return: 0 Always (SUCCESS)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
