96535127a777e0f03499f7cd2cb142ffa1be13c3
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-20 15:25:09 -0700

    prints the alphabet, in lowercase ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets
+ * function a program that prints lowercase alpha letters ten times.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
+}
