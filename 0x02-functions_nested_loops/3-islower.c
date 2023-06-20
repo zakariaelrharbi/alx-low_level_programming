a564461932439f08dae25cc3025c25ca915d2265
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-20 16:37:29 -0700

    function that checks for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,12 +7,12 @@
  */
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (0);
-	}
-	else if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
