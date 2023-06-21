da64133e4b1d4bfb937c49a2b345093a947d3eed
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-20 17:59:58 -0700

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - the longest day
+ *
+ * Return: 0
+*/
+void jack_bauer(void)
+{
+	int a, b;
+
+	a = 0;
+	while (a < 24)
+	{
+		b = a;
+		while (b < 60)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
+}
