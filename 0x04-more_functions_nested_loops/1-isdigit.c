a129daee52d4ff0bea3dc2120ea83af058e0f8e5
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-22 12:32:57 -0700

    function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - return 1 if the character is a uppercase, and 0 otherwise.
+ * @c: the number to be checked
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
