24460b9c9259a0461a8cb13a69bb8849dcbf9024
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-06-26 09:51:32 -0700

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _puts -  function that prints a string to stdout.
+ *  @str: pointer to string
+ *  Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
