dabfe34f395a0fa91f9f233b4a5b70f63354a3cb
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-13 22:11:31 +0100

    function that allocates

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,15 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
- * main - check the code
+ * malloc_checked - Allocates memory and checks if it was successful
+ * @b: The size of the memory to allocate
  *
- * Return: Always 0.
+ * Description: This function tries to allocate a block of memory of size 'b'.
+ * If it fails to do so, it exits the program with a status value of 98.
+ * If successful, it returns a pointer to the allocated memory.
+ *
+ * Return: Pointer to the allocated memory. If malloc fails, the program
+ * is terminated with a status value.
  */
 void *malloc_checked(unsigned int b)
 {
