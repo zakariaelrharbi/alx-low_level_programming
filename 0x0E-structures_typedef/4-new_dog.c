c18284f63a1bdf8789f2b4c6ed74ccddc245c477
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-17 18:45:43 +0100

    function that prints a struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,7 +51,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 	d->name = _strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	d->owner = _strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
 	return (d);
 }
