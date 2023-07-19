bbf99c59fcd7334952b34edffda81ab07a9b2f43
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-07-19 21:39:15 +0100

    pointer to function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL)
 	{
-		action(array[i]);
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
