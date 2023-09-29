0fa581e67da954040a8876472c3926d383a953dd
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-09-29 20:55:27 +0100

    betty fixed

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - Entry point of the program to check system endianness.
+ *
+ * Description:
+ * This program determines whether the system is Little Endian or Big Endian
+ * and prints the result accordingly.
+ *
+ * Return: 0 on success.
+ */
 
 int main(void)
 {
-    int n;
+	int n;
 
-    n = get_endianness();
-    if (n != 0)
-    {
-        printf("Little Endian\n");
-    }
-    else
-    {
-        printf("Big Endian\n");
-    }
-    return (0);
+	n = get_endianness();
+	if (n != 0)
+	{
+		printf("Little Endian\n");
+	}
+	else
+	{
+		printf("Big Endian\n");
+	}
+	return (0);
 }
